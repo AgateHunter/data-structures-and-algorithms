82671c81259682f14a906a0eeae5d542e8a104e9
NATALIE STONE <NATALIE.I.STONE@OUTLOOK.COM>
2023-11-27 12:01:04 -0800

    Solved challenge 1

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -10,6 +10,16 @@ Use `forEach` to loop over the input array and work with each value.  Push the n
 
 const addOne = (arr) => {
   // Solution code here...
+
+  let newArray = [];
+
+  arr.forEach( function(value) {
+
+    newArray.push( value + 1 );
+
+  });
+
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
